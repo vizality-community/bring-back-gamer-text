f1574a7d09fdf0ba24503088014aa894a360235c
dperolio <dustin.perolio@outlook.com>
2020-12-29 16:28:44 -0500

    Switch to api import

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { Messages } from '@vizality/i18n';
 import { Plugin } from '@vizality/core';
+import api from '@vizality/api';
 
 import * as i18n from './i18n';
 
 export default class BringBackGamerTexts extends Plugin {
   onStart () {
-    vizality.api.i18n.injectAllStrings(i18n);
+    api.i18n.injectAllStrings(i18n);
 
     // Strings that are the same in every language
     Messages.VERIFICATION_LEVEL_HIGH = '(╯°□°）╯︵ ┻━┻';
