8b939da2d29fca91dc93c2a0a55aa2dceb9ee2f6
dperolio <dustin.perolio@outlook.com>
2021-01-08 22:24:42 -0500

    Change @vizality/core to @vizality/entities, remove @vizality/api

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+import { Plugin } from '@vizality/entities';
 import { Messages } from '@vizality/i18n';
-import { Plugin } from '@vizality/core';
-import api from '@vizality/api';
 
 import * as i18n from './i18n';
 
 export default class BringBackGamerTexts extends Plugin {
   onStart () {
-    api.i18n.injectAllStrings(i18n);
+    vizality.api.i18n.injectAllStrings(i18n);
 
     // Strings that are the same in every language
     Messages.VERIFICATION_LEVEL_HIGH = '(╯°□°）╯︵ ┻━┻';
