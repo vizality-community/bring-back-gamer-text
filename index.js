96ac242d0c98ab70c17070ee2083330c124866b4
dperolio <dustin.perolio@outlook.com>
2021-01-13 13:24:56 -0500

    Update to start

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Messages } from '@vizality/i18n';
 import * as i18n from './i18n';
 
 export default class BringBackGamerTexts extends Plugin {
-  onStart () {
+  start () {
     vizality.api.i18n.injectAllStrings(i18n);
 
     // Strings that are the same in every language
